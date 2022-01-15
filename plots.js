df25334a4963b996a0c5ba42d9c97a99dfd7af10
danielle-askew <89716711+danielle-askew@users.noreply.github.com>
2022-01-15 16:36:37 -0700

    Add files via upload

--- plots.js
+++ plots.js
@@ -54,19 +54,22 @@ function buildMetadata(sample) {
 }
 
 // 1. Create the buildCharts function.
-function buildCharts(sample) {
+function buildBarChart(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var samples = data.samples;
+   var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-   var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+   var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-   var result = resultArray[0];
+   var result = sampleArray[0];
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
    var otu_ids = result.otu_ids;
    var otu_labels = result.otu_labels
    var sample_values = result.sample_values
+   console.log(otu_ids)
+   console.log(otu_labels)
+   console.log(sample_values)
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
@@ -97,9 +100,14 @@ function buildCharts(sample) {
 }
 // Bar and Bubble charts
 // Create the buildCharts function.
-function buildCharts(sample) {
+function buildBubbleChart(sample) {
   // Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
+    var resultArray = data
+        .samples
+        .filter(sampleObj => {
+          return sampleObj.id == sample
+        });
     
 
     // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
@@ -125,13 +133,18 @@ function buildCharts(sample) {
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot(); 
+    Plotly.newPlot('bubble', data, bubbleLayout); 
   });
 }
 // Create the buildChart function.
-function buildCharts(sample) {
+function buildGaugeChart(sample) {
   // Use d3.json to load the samples.json file 
   d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata
+    .filter(sampleObj => {
+      return sampleObj.id == sample
+      });
     console.log(data);
     
     // 3. Create a variable that holds the washing frequency.
@@ -139,15 +152,41 @@ function buildCharts(sample) {
      
     // 4. Create the trace for the gauge chart.
     var gaugeData = [
-     
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: wash_freq,
+        title: {text: "Belly Button Washing Frequency <br> Scrubs per Week", font: {size: 18}},
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { range: [null, 10]},
+          bar: { color: "steelblue" },
+          steps: [
+            { range: [0, 1], color: 'rgba(0, 0, 0, 0.5)' },
+            { range: [1, 2], color: 'rgba(0, 0, 0, 0.5)' },
+            { range: [2, 3], color: 'rgba(183,28,28, .5)' },
+            { range: [3, 4], color: 'rgba(183,28,28, .5)' },
+            { range: [4, 5], color: 'rgba(249, 168, 37, .5)' },
+            { range: [5, 6], color: 'rgba(249, 168, 37, .5)' },
+            { range: [6, 7], color: 'rgba(110, 154, 22, .5)' },
+            { range: [7, 8], color: 'rgba(110, 154, 22, .5)' },
+            { range: [8, 9], color: 'rgba(14, 127, 0, .5)' },
+            { range: [9, 10], color: 'rgba(14, 127, 0, .5)' }
+          ],
+        }  
+      }
     ];
+     
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
+      width: 600, 
+      height: 500, 
+      margin: { t: 0, b: 0 }
      
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot();
+    Plotly.newPlot('gauge', gauge_trace, gauge_layout);
   });
 }
